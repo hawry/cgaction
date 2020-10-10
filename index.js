9d73f6b54841184e7638e691c32761159eaf6743
hawry <tomas@hawry.net>
2020-10-10 14:27:31 +0200

    add env

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const core = require("@actions/core")
 const github = require("@actions/github")
 
 try {
+  
   const api_key = core.getInput("api_key")
   const template = core.getInput("template")
-
+  
   console.log("using " + template + " as input")
   console.log("using " + api_key + " as key")
-
+  
   core.setOutput("diagram", "thisissupposedtobeadiagram")
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(payload)  
+  console.log(process.env.GITHUB_TOKEN)
 } catch (error) {
   core.setFailed(error.message)
 }
