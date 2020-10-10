6110464d1cf93ca2e7a538f7a39372d9278b1a4b
hawry <tomas@hawry.net>
2020-10-10 15:10:43 +0200

    remove outputs

--- index.js
+++ index.js
@@ -3,30 +3,7 @@ const github = require("@actions/github")
 const { exec } = require("child_process");
 const { promises: fs } = require("fs")
 
-const ls = (dir) => {
-  exec("ls -la " + dir, (error, stdout, stderr) => {
-    if (error) {
-      console.log(`error: ${error.message}`)
-      return
-    }
-
-    if (stderr) {
-      console.log(`stderr: ${stderr}`)
-      return
-    }
-
-    console.log(`stdout: ${stdout}`)
-  })
-}
-
 const main = async () => {
-
-  ls("./")
-  ls("/github/")
-  ls("/github/home")
-  ls("/github/workspaces")
- 
-
   const api_key = core.getInput("api_key")
   const template = core.getInput("template")
   
@@ -37,9 +14,6 @@ const main = async () => {
   console.log(content)
 
   core.setOutput("diagram", "thisissupposedtobeadiagram")
-
-
-  await fs.writeFile(core.getInput("output_file"), "this is the xml stuff you know")
 }
 
 main().catch(err => core.setFailed(err.message))
