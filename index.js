fbd5d92c0a7a40da8de5fbd1645092b8423588ea
hawry <tomas@hawry.net>
2020-10-10 14:37:24 +0200

    added output file

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const core = require("@actions/core")
 const github = require("@actions/github")
 
-try {
-  
+const { promises: fs } = require("fs")
+
+
+const main = async () => {
+
   const api_key = core.getInput("api_key")
   const template = core.getInput("template")
   
   console.log("using " + template + " as input")
   console.log("using " + api_key + " as key")
-  
+
+  const content = await fs.readFile(template, 'utf8')
+  console.log(content)
+
   core.setOutput("diagram", "thisissupposedtobeadiagram")
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(payload)  
-  console.log(process.env.GITHUB_TOKEN)
-} catch (error) {
-  core.setFailed(error.message)
+
+
+  await fs.writeFile(core.getInput("output_file"), "this is the xml stuff you know")
 }
+
+main().catch(err => core.setFailed(err.message))
