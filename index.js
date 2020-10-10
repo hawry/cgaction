14bd9691ce26beaa1a19ae94f29104c0f7e599be
hawry <tomas@hawry.net>
2020-10-10 16:37:42 +0200

    add axios

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 const core = require("@actions/core")
 const github = require("@actions/github")
-const { exec } = require("child_process");
 const { promises: fs } = require("fs")
+const zlib = require("zlib")
+
+const axios = require('axios')
+
 
 const main = async () => {
-  const api_key = core.getInput("api_key")
-  const template = core.getInput("template")
-  
-  console.log("using " + template + " as input")
-  console.log("using " + api_key + " as key")
-
+  // const template = core.getInput("template")
+  const template = "example.yml"
   const content = await fs.readFile(template, 'utf8')
-  console.log(content)
+  // console.log(content)
+
+  let deflated = await zlib.deflateSync(content).toString('base64')
+  console.log(deflated)
+
+  axios.post(
+    "https://cg.deforest.io/live/convert",
+    {
+      data: deflated,
+    }
+  ).then(res => {
+    let inflated = zlib.inflateSync(Buffer.from(res.data.data, 'base64')).toString()
+    console.log(inflated)
+  }).catch(err => {
+    console.log(err.bo)
+  })
 
   core.setOutput("diagram", "thisissupposedtobeadiagram")
 }
