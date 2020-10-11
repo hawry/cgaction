785132624f344e8e49b0708318d979199b764b18
hawry <tomas@hawry.net>
2020-10-11 10:00:25 +0200

    basic working action

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 const core = require("@actions/core")
-const github = require("@actions/github")
 const { promises: fs } = require("fs")
 const zlib = require("zlib")
-
 const axios = require('axios')
 
-
 const main = async () => {
-  // const template = core.getInput("template")
-  const template = "example.yml"
+  const template = core.getInput("template")
   const content = await fs.readFile(template, 'utf8')
-  // console.log(content)
-
   let deflated = await zlib.deflateSync(content).toString('base64')
-  console.log(deflated)
-
   axios.post(
     "https://cg.deforest.io/live/convert",
     {
@@ -22,12 +14,11 @@ const main = async () => {
     }
   ).then(res => {
     let inflated = zlib.inflateSync(Buffer.from(res.data.data, 'base64')).toString()
-    console.log(inflated)
+    core.setOutput("diagram", inflated)
   }).catch(err => {
     console.log(err.bo)
   })
 
-  core.setOutput("diagram", "thisissupposedtobeadiagram")
 }
 
 main().catch(err => core.setFailed(err.message))
